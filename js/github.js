c325de27fe2d7e3ba49f576bf13bdb4f8e55d904
brizism <montielbriseida@gmail.com>
2018-02-17 00:46:20 -0500

    repos pagination and sort

--- js/github.js
+++ js/github.js
@@ -2,15 +2,21 @@ class Github {
   constructor(){
     this.client_id = '877ab5ad70a67cf66c32';
     this.client_secret = 'c05682db2818c8b5655b89149635f75d9911340c';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user){
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
