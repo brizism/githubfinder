f5d3bda805633540359e76d4a895bb38cb8eb69d
brizism <montielbriseida@gmail.com>
2018-02-16 21:37:34 -0500

    edit fetch endpoint

--- js/github.js
+++ js/github.js
@@ -1,11 +1,11 @@
 class Github {
   constructor(){
     this.client_id = '877ab5ad70a67cf66c32';
-    this.client_secret = '5ef5c5fa435257a762cf90cafa8c0c63453f6761';
+    this.client_secret = 'c05682db2818c8b5655b89149635f75d9911340c';
   }
 
   async getUser(user){
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret${this.client_secret}`);
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     const profile = await profileResponse.json();
 
